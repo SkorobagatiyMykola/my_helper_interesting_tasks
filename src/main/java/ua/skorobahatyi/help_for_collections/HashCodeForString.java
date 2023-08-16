5b4bd44370a51cd01064963b8f5ee17b162e90ee
SkorobagatiyMykola <nick1980457@gmail.com>
2023-08-16 21:20:53 +0300

    Explain hashCode()

---
+++ src/main/java/ua/skorobahatyi/help_for_collections/HashCodeForString.java
@@ -0,0 +1,111 @@
+package ua.skorobahatyi.help_for_collections;
+
+public class HashCodeForString {
+    private static final String allSymbols = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}";
+
+    public static void main(String[] args) {
+        //iterateAll_1_Digit_Values();
+        //iterateAll_2_Digit_Values();
+       // iterateAll_3_Digit_Values();
+        iterateAll_4_Digit_Values();
+       // iterateAll_5_Digit_Values();
+        /*iterateAll_6_Digit_Values();*/
+        System.out.println("   ".hashCode());
+        System.out.println(Integer.MAX_VALUE);
+    }
+
+    private static void printAllSymbolsFromANSI() {
+        for (int i = 33; i < 126; i++) {
+            System.out.print(Character.toString(i));
+        }
+    }
+
+    private static void iterateAll_1_Digit_Values() {
+        for (int i = 0; i < allSymbols.length(); i++) {
+            String check = ""+allSymbols.charAt(i);
+            if (check.hashCode() == 0)
+                System.out.println(check);
+        }
+    }
+
+    private static void iterateAll_2_Digit_Values() {
+        for (int i = 0; i < allSymbols.length(); i++) {
+            for (int j = 0; j < allSymbols.length(); j++) {
+                String check = ""+allSymbols.charAt(i) + allSymbols.charAt(j);
+                if (check.hashCode() == 0) {
+                    System.out.println(check);
+                }
+                System.out.println(check+" - "+check.hashCode());
+            }
+        }
+    }
+
+    private static void iterateAll_3_Digit_Values() {
+        for (int i = 0; i < allSymbols.length(); i++) {
+            for (int j = 0; j < allSymbols.length(); j++) {
+                for (int k = 0; k < allSymbols.length(); k++) {
+                    String check = ""+allSymbols.charAt(i) +
+                            allSymbols.charAt(j) + allSymbols.charAt(k);
+                    if (check.hashCode() == 0)
+                        System.out.println(check);
+                }
+            }
+        }
+    }
+
+    private static void iterateAll_4_Digit_Values() {
+        for (int i = 0; i < allSymbols.length(); i++) {
+            for (int j = 0; j < allSymbols.length(); j++) {
+                for (int k = 0; k < allSymbols.length(); k++) {
+                    for (int l = 0; l < allSymbols.length(); l++) {
+                        String check = ""+allSymbols.charAt(i) +
+                                allSymbols.charAt(j) + allSymbols.charAt(k) + allSymbols.charAt(l);
+                        if (check.hashCode() == 0)
+                            System.out.println(check);
+                        System.out.println(check+" - "+check.hashCode());
+                    }
+
+                }
+            }
+        }
+    }
+
+    private static void iterateAll_5_Digit_Values() {
+        for (int i = 0; i < allSymbols.length(); i++) {
+            for (int j = 0; j < allSymbols.length(); j++) {
+                for (int k = 0; k < allSymbols.length(); k++) {
+                    for (int l = 0; l < allSymbols.length(); l++) {
+                        for (int m = 0; m < allSymbols.length(); m++) {
+                            String check = ""+allSymbols.charAt(i) +
+                                    allSymbols.charAt(j) + allSymbols.charAt(k) +
+                                    allSymbols.charAt(l) + allSymbols.charAt(m);
+                            if (check.hashCode() == 0)
+                                System.out.println(check);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static void iterateAll_6_Digit_Values() {
+        for (int i = 0; i < allSymbols.length(); i++) {
+            for (int j = 0; j < allSymbols.length(); j++) {
+                for (int k = 0; k < allSymbols.length(); k++) {
+                    for (int l = 0; l < allSymbols.length(); l++) {
+                        for (int m = 0; m < allSymbols.length(); m++) {
+                            for (int n = 0; n < allSymbols.length(); n++) {
+                                String check = ""+allSymbols.charAt(i) +
+                                        allSymbols.charAt(j) + allSymbols.charAt(k) +
+                                        allSymbols.charAt(l) + allSymbols.charAt(m) + allSymbols.charAt(n);
+                                if (check.hashCode() == 0)
+                                    System.out.println(check);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/ua/skorobahatyi/help_for_collections/HelpHashCode.java
@@ -0,0 +1,27 @@
+package ua.skorobahatyi.help_for_collections;
+
+public class HelpHashCode {
+    public static void main(String[] args) {
+        Integer i=4;
+        Integer i2=4456;
+        Integer i3=487985415;
+        System.out.println(i.hashCode());
+        System.out.println(i2.hashCode());
+        System.out.println(i3.hashCode());
+
+        ////////////////
+        Object obj = new Object();
+        System.out.println(obj.hashCode());
+        User user = new User(1,"Nick","Skorobagatiy",43,"nick@gmail.com");
+        System.out.println(user.hashCode());
+
+        String name="Nick";
+        System.out.println(name.hashCode());
+
+        Double d1= Double.valueOf(1.25);
+        System.out.println(d1.hashCode());
+
+        Long l1= 12356865181556554l;
+        System.out.println(l1.hashCode());
+    }
+}
