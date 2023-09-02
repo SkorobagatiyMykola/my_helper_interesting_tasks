998a05d8511b595193169275e852446d6ea1693a
SkorobagatiyMykola <nick1980457@gmail.com>
2023-09-02 12:02:43 +0300

    Solved problem sql-tasks

--- src/main/java/ua/skorobahatyi/tasks_from_leetcode/mysql/easy_scripts.sql
+++ src/main/java/ua/skorobahatyi/tasks_from_leetcode/mysql/easy_scripts.sql
@@ -10,3 +10,9 @@ select distinct v.author_id id from Views v where v.author_id=v.viewer_id order 
 select a.activity_date day, count(distinct user_id) active_users from Activity a
           where a.activity_date between '2019-06-27' and '2019-07-27'
           group by a.activity_date;
+--182. Duplicate Emails
+--https://leetcode.com/problems/duplicate-emails/
+SELECT p.email Email FROM Person p GROUP BY p.email HAVING count(p.email)>=2;
+--183. Customers Who Never Order
+--https://leetcode.com/problems/customers-who-never-order/description/
+SELECT c.name Customers FROM Customers c LEFT JOIN Orders o ON c.id=o.customerId WHERE o.customerId IS NULL;
