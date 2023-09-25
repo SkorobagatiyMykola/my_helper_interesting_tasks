17b80b83f7d598d4aefcf91d4a48fd0fb4f79f4c
SkorobagatiyMykola <nick1980457@gmail.com>
2023-09-25 11:57:36 +0300

    Solved sql-task (Product Sales Analysis III)

--- src/main/java/ua/skorobahatyi/tasks_from_leetcode/mysql/medium_scripts.sql
+++ src/main/java/ua/skorobahatyi/tasks_from_leetcode/mysql/medium_scripts.sql
@@ -1,3 +1,8 @@
 --1045. Customers Who Bought All Products
 --https://leetcode.com/problems/customers-who-bought-all-products/description/?envType=study-plan-v2&envId=top-sql-50
 SELECT c.customer_id FROM Customer c GROUP BY c.customer_id HAVING  COUNT(DISTINCT c.product_key)=(SELECT count(*) FROM Product)
+--1070. Product Sales Analysis III
+--https://leetcode.com/problems/product-sales-analysis-iii/description/?envType=study-plan-v2&envId=top-sql-50
+SELECT s.product_id, s.year first_year, s.quantity, s.price FROM Sales s INNER JOIN
+      (SELECT s.product_id, MIN(s.year) year FROM Sales s GROUP BY product_id) ss
+        ON s.product_id=ss.product_id AND s.year=ss.year
