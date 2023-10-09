6edcd571a7c4f4104ab09bd1bcd0ab368d8ddfd1
SkorobagatiyMykola <nick1980457@gmail.com>
2023-10-09 17:09:20 +0300

    Solved js-task (Counter II)

---
+++ src/main/java/ua/skorobahatyi/tasks_from_leetcode/js_scripts/easy/counter2.js
@@ -0,0 +1,25 @@
+//2665. Counter II
+//https://leetcode.com/problems/counter-ii/description/?envType=study-plan-v2&envId=30-days-of-javascript
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let t = {
+        val: init,
+        increment: ()=>++t.val,
+        decrement: ()=>--t.val,
+        reset: ()=>{
+            t.val=init
+            return t.val
+        }
+    }
+    return t
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
