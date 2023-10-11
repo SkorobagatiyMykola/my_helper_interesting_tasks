cddca6a9a15f696ebb4721097ec648415ec764af
SkorobagatiyMykola <nick1980457@gmail.com>
2023-10-11 18:06:01 +0300

    Solved java-task (Function Composition)

--- src/main/java/ua/skorobahatyi/tasks_from_leetcode/js_scripts/easy/easy_srcipts.js
+++ src/main/java/ua/skorobahatyi/tasks_from_leetcode/js_scripts/easy/easy_srcipts.js
@@ -3,3 +3,15 @@
 var isEmpty = function(obj) {
     return obj===null || Object.keys(obj).length===0 ? true : false;
 };
+//2629. Function Composition
+//https://leetcode.com/problems/function-composition/description/?envType=study-plan-v2&envId=30-days-of-javascript
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+
+	return function(x) {
+        return functions.reduceRight((y,fn)=>  fn(y),x)
+    }
+};
