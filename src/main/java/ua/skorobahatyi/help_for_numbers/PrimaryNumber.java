074737927315a61206e43926dff016083018a8da
SkorobagatiyMykola <nick1980457@gmail.com>
2023-10-31 22:02:32 +0200

    Solved java-task (Primary Numbers)

--- src/main/java/ua/skorobahatyi/help_for_numbers/PrimaryNumber.java
+++ src/main/java/ua/skorobahatyi/help_for_numbers/PrimaryNumber.java
@@ -9,7 +9,7 @@ import java.util.List;
  * Find the largest palindrome which is product of two 5-digit prime numbers.
  */
 
-public class TaskPrimaryNumber {
+public class PrimaryNumber {
 
     private static int[] arrayPrimaryNumberForCheck = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
             61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
