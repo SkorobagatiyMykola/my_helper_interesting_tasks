d5ad8912300eb82846dfdf3173e04b1472a48296
SkorobagatiyMykola <nick1980457@gmail.com>
2023-11-01 21:37:13 +0200

    Solved java-task (ChocolateFeast)

---
+++ src/main/java/ua/skorobahatyi/tasks_from_hackerrank/good_alg/ChocolateFeast.java
@@ -0,0 +1,32 @@
+package ua.skorobahatyi.tasks_from_hackerrank.good_alg;
+
+/**
+ * Chocolate Feast
+ * https://www.hackerrank.com/challenges/chocolate-feast/problem?isFullScreen=true&utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ * */
+public class ChocolateFeast {
+
+    public static void main(String[] args) {
+
+        chocolateFeast(15,3,2);
+    }
+
+
+    private static int chocolateFeast(int n, int c, int m) {
+
+        int count=0,s=0,l=0;
+        while(n>0|| s>=m) {
+            if (n>0){
+                s=n/c;
+                n=0;
+                count+=s;
+            }
+            l=s/m;
+            count+=l;
+            s=l+(s%m);
+
+        }
+        System.out.println("Numbers chocolate: "+count);
+        return count;
+    }
+}
