53a42dfc96ce896444b5371ea9a656989c054119
SkorobagatiyMykola <nick1980457@gmail.com>
2023-11-18 19:23:07 +0200

    Solved java- task LeonardosPrimeFactors

---
+++ src/main/java/ua/skorobahatyi/tasks_from_hackerrank/good_algorithms/easy/LeonardosPrimeFactors.java
@@ -0,0 +1,11 @@
+package ua.skorobahatyi.tasks_from_hackerrank.good_algorithms.easy;
+
+/**
+ * https://www.hackerrank.com/challenges/leonardo-and-prime/problem?isFullScreen=true
+ * */
+public class LeonardosPrimeFactors {
+
+    public static void main(String[] args) {
+
+    }
+}
