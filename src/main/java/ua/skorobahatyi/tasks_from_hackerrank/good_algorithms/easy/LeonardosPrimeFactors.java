82d097e4941def615403e5bada774955951f9baa
SkorobagatiyMykola <nick1980457@gmail.com>
2023-11-19 20:10:56 +0200

    Updated java-tasks (Leonardos)

--- src/main/java/ua/skorobahatyi/tasks_from_hackerrank/good_algorithms/easy/LeonardosPrimeFactors.java
+++ src/main/java/ua/skorobahatyi/tasks_from_hackerrank/good_algorithms/easy/LeonardosPrimeFactors.java
@@ -6,6 +6,20 @@ package ua.skorobahatyi.tasks_from_hackerrank.good_algorithms.easy;
 public class LeonardosPrimeFactors {
 
     public static void main(String[] args) {
+        System.out.println(primeCount(1));
+//        System.out.println(primeCount(2));
+//        System.out.println(primeCount(3));
+//        System.out.println(primeCount(500));
+//        System.out.println(primeCount(5000));
+//        System.out.println(primeCount(10000000000l));
+    }
 
+    private static int primeCount(long n) {
+        // Write your code here
+        int[] arrayPrimes={2,3,5,7,11,13,};
+        long z=0;
+        //while ()
+
+        return 0;
     }
 }
