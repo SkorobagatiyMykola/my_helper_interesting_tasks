4b0e420f9cca8ddff47b213bb8504bb95cf673ee
SkorobagatiyMykola <nick1980457@gmail.com>
2023-11-20 17:45:14 +0200

    Solved java-problem PalindromeNumber(update)

--- src/main/java/ua/skorobahatyi/help_for_numbers/PalindromeNumber.java
+++ src/main/java/ua/skorobahatyi/help_for_numbers/PalindromeNumber.java
@@ -1,10 +1,15 @@
 package ua.skorobahatyi.help_for_numbers;
 
 /**
- *  Polindrome
- *
- * */
+ * Polindrome
+ */
 public class PalindromeNumber {
+    public static void main(String[] args) {
+        PalindromeNumber pl = new PalindromeNumber();
+        System.out.println(pl.countDigits(100));
+        System.out.println(pl.countDigits(5));
+        System.out.println(pl.countDigits(1234567890));
+    }
 
     public boolean isPalindrome(long number) {
 
@@ -20,6 +25,15 @@ public class PalindromeNumber {
         return temp == sum ? true : false;
     }
 
+    public int countDigits(long number) {
+
+        int digits = 0;
+        while (number > 0) {
+            number = number / 10;
+            digits++;
+        }
+        return digits;
+    }
 
 
 }
