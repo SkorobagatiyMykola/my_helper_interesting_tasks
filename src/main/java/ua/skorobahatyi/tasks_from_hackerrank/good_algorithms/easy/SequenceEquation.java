6a6ed71e3936d31ee025c8f32efeda56e081b4d1
SkorobagatiyMykola <nick1980457@gmail.com>
2023-11-24 17:06:53 +0200

    Solved java-task for 2 solution StrongPassword

---
+++ src/main/java/ua/skorobahatyi/tasks_from_hackerrank/good_algorithms/easy/SequenceEquation.java
@@ -0,0 +1,21 @@
+package ua.skorobahatyi.tasks_from_hackerrank.good_algorithms.easy;
+
+import java.util.List;
+
+/**
+ *
+ * */
+public class SequenceEquation {
+    public static void main(String[] args) {
+
+    }
+
+    private static List<Integer> permutationEquation(List<Integer> p) {
+        // Write your code here
+        for (int i = 0; i < p.get(i); i++) {
+
+        }
+
+        return null;
+    }
+}
