69c15cbc3781afbab9f06260b054921e52a9545d
SkorobagatiyMykola <nick1980457@gmail.com>
2023-11-25 21:08:56 +0200

    Solved problem with Gson + Jackson

--- src/main/java/ua/skorobahatyi/tasks_from_linked/http_response/Used_Jackson.java
+++ src/main/java/ua/skorobahatyi/tasks_from_linked/http_response/Used_Jackson.java
@@ -1,6 +1,17 @@
 package ua.skorobahatyi.tasks_from_linked.http_response;
 
+/**
+ *https://jsonmock.hackerrank.com/api/medical_records
+ *https://www.transtutors.com/questions/rest-api-fit-count-use-the-http-get-method-to-retrieve-information-from-a--9588591.htm
+ *https://gist.github.com/vipul-zambare006/d8aa23e212da2db2625d08aa9bdba588
+ * https://medium.com/@sunshine990316/mckinsey-data-engineer-oa-prepare-595db6d397db
+ *
+ *
+ * https://www.hackerrank.com/apply
+ *
+ */
 public class Used_Jackson {
+    private static String URL = "https://jsonmock.hackerrank.com/api/medical_records";
     public static void main(String[] args) {
 
     }
