4bb4db7519b8551bc0bc984d47a40451fcf8e147
SkorobagatiyMykola <nick1980457@gmail.com>
2023-11-26 19:29:50 +0200

    Updated Used_Gson

--- src/main/java/ua/skorobahatyi/tasks_from_linked/http_response/Used_Gson.java
+++ src/main/java/ua/skorobahatyi/tasks_from_linked/http_response/Used_Gson.java
@@ -1,6 +1,7 @@
 package ua.skorobahatyi.tasks_from_linked.http_response;
 
 public class Used_Gson {
+    private static String URL = "https://jsonmock.hackerrank.com/api/medical_records";
     public static void main(String[] args) {
 
     }
