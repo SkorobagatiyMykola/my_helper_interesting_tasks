57d124ad5cb2296f9b7f4e77d7f8c773aec2f3a4
SkorobagatiyMykola <nick1980457@gmail.com>
2023-12-19 18:41:40 +0200

    Solved java-tasks FlatlandSpaceStations

---
+++ src/main/java/ua/skorobahatyi/tasks_from_hackerrank/easy_alg/FlatlandSpaceStations.java
@@ -0,0 +1,14 @@
+package ua.skorobahatyi.tasks_from_hackerrank.easy_alg;
+/**
+ * https://www.hackerrank.com/challenges/flatland-space-stations/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ * */
+public class FlatlandSpaceStations {
+    public static void main(String[] args) {
+
+    }
+
+    private static int flatlandSpaceStations(int n, int[] c) {
+
+        return 0;
+    }
+}
