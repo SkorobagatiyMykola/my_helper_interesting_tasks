b412188d888aa221a9c9bf4479c9ca5aea90ef10
SkorobagatiyMykola <nick1980457@gmail.com>
2023-12-29 17:53:38 +0200

    New java-task ArrayManipulation

---
+++ src/main/java/ua/skorobahatyi/tasks_from_hackerrank/good_algorithms/hard/ArrayManipulation.java
@@ -0,0 +1,11 @@
+package ua.skorobahatyi.tasks_from_hackerrank.good_algorithms.hard;
+
+/**
+ * todo
+ * https://www.hackerrank.com/challenges/crush/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ * */
+public class ArrayManipulation {
+    public static void main(String[] args) {
+
+    }
+}
