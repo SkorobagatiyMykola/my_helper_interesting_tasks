bcdf10b2f7dd5960fb7cb6d3b4447ba5ebc91b4a
SkorobagatiyMykola <nick1980457@gmail.com>
2024-01-02 16:56:54 +0200

    Solved java-task Dynamic

---
+++ src/main/java/ua/skorobahatyi/tasks_from_hackerrank/good_algorithms/easy/DynamicArray.java
@@ -0,0 +1,21 @@
+package ua.skorobahatyi.tasks_from_hackerrank.good_algorithms.easy;
+
+import java.util.List;
+
+/**
+ *
+ * todo
+ * https://www.hackerrank.com/challenges/dynamic-array/problem?isFullScreen=true&utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ * */
+public class DynamicArray {
+    public static void main(String[] args) {
+
+    }
+
+    private static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
+        // Write your code here
+
+        return null;
+    }
+
+}
