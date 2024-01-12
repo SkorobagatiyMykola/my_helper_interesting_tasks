03038d192cfeaf561b1e32d96e65168ed3707c71
SkorobagatiyMykola <nick1980457@gmail.com>
2024-01-12 15:56:18 +0200

    Solved java-task ViralAdvertising

---
+++ src/main/java/ua/skorobahatyi/tasks_from_hackerrank/easy_alg/ViralAdvertising.java
@@ -0,0 +1,14 @@
+package ua.skorobahatyi.tasks_from_hackerrank.easy_alg;
+
+public class ViralAdvertising {
+    public static void main(String[] args) {
+
+        System.out.println(viralAdvertising(2));
+    }
+
+     public static int viralAdvertising(int n) {
+        // Write your code here
+
+         return 0;
+    }
+}
