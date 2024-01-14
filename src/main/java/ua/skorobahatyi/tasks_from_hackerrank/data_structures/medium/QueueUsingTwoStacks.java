f3ffcf9741007884dd24917c8ba24fffa9e21891
SkorobagatiyMykola <nick1980457@gmail.com>
2024-01-14 19:25:00 +0200

    Added new java-task

---
+++ src/main/java/ua/skorobahatyi/tasks_from_hackerrank/data_structures/medium/QueueUsingTwoStacks.java
@@ -0,0 +1,15 @@
+package ua.skorobahatyi.tasks_from_hackerrank.data_structures.medium;
+
+
+/**
+ * https://www.hackerrank.com/challenges/queue-using-two-stacks/problem?isFullScreen=true&utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ *
+ * todo (!!!)
+ * */
+public class QueueUsingTwoStacks {
+    public static void main(String[] args) {
+
+    }
+
+
+}
