6581a1a1037524224be42f161f157853543a1428
SkorobagatiyMykola <nick1980457@gmail.com>
2024-01-16 15:20:47 +0200

    Added java-task SimpleTextEditor

---
+++ src/main/java/ua/skorobahatyi/tasks_from_hackerrank/data_structures/easy/SimpleTextEditor.java
@@ -0,0 +1,14 @@
+package ua.skorobahatyi.tasks_from_hackerrank.data_structures.easy;
+
+/**
+ *
+ * https://www.hackerrank.com/challenges/simple-text-editor/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ * todo (???)
+ * */
+public class SimpleTextEditor {
+    public static void main(String[] args) {
+
+    }
+
+
+}
