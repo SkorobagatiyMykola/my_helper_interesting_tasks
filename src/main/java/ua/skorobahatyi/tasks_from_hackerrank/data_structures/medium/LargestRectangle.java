48ce60d49953a32dc30412990a99a80fa4caa83b
SkorobagatiyMykola <nick1980457@gmail.com>
2024-01-17 16:15:05 +0200

    Solved java-task LargestRectangle

---
+++ src/main/java/ua/skorobahatyi/tasks_from_hackerrank/data_structures/medium/LargestRectangle.java
@@ -0,0 +1,13 @@
+package ua.skorobahatyi.tasks_from_hackerrank.data_structures.medium;
+
+
+/**
+ * https://www.hackerrank.com/challenges/largest-rectangle/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ * todo (???)
+ * */
+public class LargestRectangle {
+
+    public static void main(String[] args) {
+
+    }
+}
