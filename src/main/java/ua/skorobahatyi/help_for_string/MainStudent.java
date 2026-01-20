a12fe91690feadd3addb16a07c82f58ff06d1030
SkorobagatiyMykola <nick1980457@gmail.com>
2026-01-20 12:43:08 +0200

    Tested Student class

---
+++ src/main/java/ua/skorobahatyi/help_for_string/MainStudent.java
@@ -0,0 +1,12 @@
+package ua.skorobahatyi.help_for_string;
+
+public class MainStudent {
+    public static void main(String[] args) {
+        Student student = new Student("Nick",45);
+        Student student1 = new Student();
+        Student student2 = new Student();
+        System.out.println(student);
+        System.out.println(student1);
+        System.out.println(student2);
+    }
+}
---
+++ src/main/java/ua/skorobahatyi/help_for_string/Student.java
@@ -0,0 +1,18 @@
+package ua.skorobahatyi.help_for_string;
+
+import jdk.jfr.Name;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private String name;
+    private int age;
+  /*  @Override
+    public String toString() {
+        return "Test";
+    }*/
+}
