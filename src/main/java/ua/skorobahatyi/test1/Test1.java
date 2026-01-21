f626a5212f9c5ee533578e8ba92a1d9f232ce9b2
SkorobagatiyMykola <nick1980457@gmail.com>
2026-01-21 17:09:05 +0200

    Added tests

---
+++ src/main/java/ua/skorobahatyi/test1/Test1.java
@@ -0,0 +1,19 @@
+package ua.skorobahatyi.test1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+    private static final List<String> arr=new ArrayList<>();
+    public static void main(String[] args) {
+
+        arr.clear();
+
+        arr.add("cdscvd");
+        arr.add("cdscvd");
+        arr.add("cdscvd");
+
+        System.out.println(arr);
+
+    }
+}
