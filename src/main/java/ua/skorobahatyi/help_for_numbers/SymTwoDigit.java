7796f35ba2f2f139110a7d133dfadc86628892ac
SkorobagatiyMykola <nick1980457@gmail.com>
2026-01-28 18:11:13 +0200

    Added first solution

--- src/main/java/ua/skorobahatyi/help_for_numbers/SymTwoDigit.java
+++ src/main/java/ua/skorobahatyi/help_for_numbers/SymTwoDigit.java
@@ -7,5 +7,26 @@ package ua.skorobahatyi.help_for_numbers;
 public class SymTwoDigit {
     public static void main(String[] args) {
         System.out.println(" ");
+        checkSolution();
+    }
+
+    private static void checkSolution(){
+        int[] arr = {123,2345,34,67,5444,100,600};
+
+        for (int num:arr) {
+            System.out.println(num+", sum digit:"+sumDigit(num));
+        }
+    }
+
+    private static int sumDigit(int num){
+        int sum =0;
+        while (num>=10){
+            int ost=num%10;
+            sum+=ost;
+            num=(num-ost)/10;
+
+        }
+        //System.out.println(num);
+        return sum+num;
     }
 }
