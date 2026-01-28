7670a04268cb35446879af88f76c80e47b58385c
SkorobagatiyMykola <nick1980457@gmail.com>
2026-01-28 16:35:56 +0200

    Added description for task

---
+++ src/main/java/ua/skorobahatyi/help_for_numbers/SymTwoDigit.java
@@ -0,0 +1,11 @@
+package ua.skorobahatyi.help_for_numbers;
+
+
+/**
+ * Знайдіть двозначне число, сума цифр якого не змінюється при множенні на будь-яке однозначне число.
+ * */
+public class SymTwoDigit {
+    public static void main(String[] args) {
+        System.out.println(" ");
+    }
+}
