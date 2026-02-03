fbeb21c69595cb7caba8a040ece2fdf4c7929eef
SkorobagatiyMykola <nick1980457@gmail.com>
2026-02-03 18:24:10 +0200

    Added first solution2

--- src/main/java/ua/skorobahatyi/help_for_numbers/SymTwoDigit.java
+++ src/main/java/ua/skorobahatyi/help_for_numbers/SymTwoDigit.java
@@ -2,6 +2,7 @@ package ua.skorobahatyi.help_for_numbers;
 
 
 /**
+ *
  * Знайдіть двозначне число, сума цифр якого не змінюється при множенні на будь-яке однозначне число.
  */
 public class SymTwoDigit {
