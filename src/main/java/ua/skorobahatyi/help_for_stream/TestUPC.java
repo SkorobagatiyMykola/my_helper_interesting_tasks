a157ecaa031de9a7d721537998efa3b2519b4c69
SkorobagatiyMykola <nick1980457@gmail.com>
2026-02-25 18:48:52 +0200

    Refactoring

--- src/main/java/ua/skorobahatyi/help_for_stream/TestUPC.java
+++ src/main/java/ua/skorobahatyi/help_for_stream/TestUPC.java
@@ -2,7 +2,7 @@ package ua.skorobahatyi.help_for_stream;
 
 import java.util.List;
 
-public class TeeestUPC {
+public class TestUPC {
     public static void main(String[] args) {
         List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
