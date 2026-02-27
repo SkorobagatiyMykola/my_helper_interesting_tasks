b7bbdec68599a6d55ac7d009e4572e357489f577
SkorobagatiyMykola <nick1980457@gmail.com>
2026-02-27 13:03:49 +0200

    Added file for decorator

---
+++ src/main/java/ua/skorobahatyi/design_patterns/structural_patterns/decorator/Developer.java
@@ -0,0 +1,5 @@
+package ua.skorobahatyi.design_patterns.structural_patterns.decorator;
+
+public interface Developer {
+    public String makeJob();
+}
---
+++ src/main/java/ua/skorobahatyi/design_patterns/structural_patterns/decorator/Task.java
@@ -0,0 +1,11 @@
+package ua.skorobahatyi.design_patterns.structural_patterns.decorator;
+
+public class Task {
+    public static void main(String[] args) {
+        //Developer developer = new JavaDeveloper();
+        //Developer developer = new SeniorJavaDeveloper(new JavaDeveloper());
+        Developer developer = new JavaTeamLead(new SeniorJavaDeveloper(new JavaDeveloper()));
+
+        System.out.println(developer.makeJob());
+    }
+}
