eb46e0ecf549c5fd17807b6c40e928294d5adaa0
SkorobagatiyMykola <nick1980457@gmail.com>
2026-03-13 16:16:55 +0200

    Added chain-of-responsibility template

---
+++ src/main/java/ua/skorobahatyi/design_patterns/behavioral_patterns/chain_of_responsibility/BudTracker.java
@@ -0,0 +1,16 @@
+package ua.skorobahatyi.design_patterns.behavioral_patterns.chain_of_responsibility;
+
+public class BudTracker {
+    public static void main(String[] args) {
+        Notifier reportNotifier = new SimpleReportNotifier(Priority.ROUTINE);
+        Notifier emailNotifier = new EmailNotifier(Priority.IMPORTANT);
+        Notifier smsNotifier = new SMSNotifier(Priority.ASAP);
+
+        reportNotifier.setNextNotifier(emailNotifier);
+        emailNotifier.setNextNotifier(smsNotifier);
+
+        reportNotifier.notifyManager("Everything is OK.", Priority.ROUTINE);
+        reportNotifier.notifyManager("Something went wrong.", Priority.IMPORTANT);
+        reportNotifier.notifyManager("Huston, we had a problem here !!!", Priority.ASAP);
+    }
+}
