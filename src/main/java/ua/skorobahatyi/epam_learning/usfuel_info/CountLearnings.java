cce0396bca8300bed8f24673bf2395957dd9318c
SkorobagatiyMykola <nick1980457@gmail.com>
2026-03-17 10:08:19 +0200

    Epam first my task

---
+++ src/main/java/ua/skorobahatyi/epam_learning/usfuel_info/CountLearnings.java
@@ -0,0 +1,101 @@
+package ua.skorobahatyi.epam_learning.usfuel_info;
+
+public class CountLearnings {
+
+    private static String leaners = "Arsen Silchenko <arsenfurte@gmail.com>,\n" +
+            "Stepan Petriv | Guest <petrivs572@gmail.com>,\n" +
+            "Alex Rezun | Guest <dp070999rav@gmail.com>,\n" +
+            "Halya Tsivka <ostnew0@gmail.com>,\n" +
+            "\"tetianavkukharchuk@gmail.com\" <tetianavkukharchuk@gmail.com>,\n" +
+            "Oleksandr Tkachuk <fsffsf1984@gmail.com>,\n" +
+            "Daria Ferbei <dariaferbey@gmail.com>,\n" +
+            "Sergey Bortnitskiy <sergey.bortnitskiy@gmail.com>,\n" +
+            "\"a.s.balan96@gmail.com\" <a.s.balan96@gmail.com>,\n" +
+            "\"dzavidnyuk@gmail.com\" <dzavidnyuk@gmail.com>,\n" +
+            "Yurii Tsudzenko | Guest <yura9989@gmail.com>,\n" +
+            "Nikolay Scorobagatov <nick1980457@gmail.com>,\n" +
+            "Serhii Skakun <skakun1978@gmail.com>,\n" +
+            "\"vvo12015@gmail.com\" <vvo12015@gmail.com>,\n" +
+            "\"jekinson1999@gmail.com\" <jekinson1999@gmail.com>,\n" +
+            "Daryna Vechtomova | Guest <vechtomova04@gmail.com>,\n" +
+            "Andrii Hyndych <gindichand@gmail.com>,\n" +
+            "Andrij Hrinin <andergrinin@gmail.com>,\n" +
+            "Kirill Kliuienko | Guest <duskmage2009@gmail.com>,\n" +
+            "Yuliia Vasylieva | Guest <qellexi@gmail.com>,\n" +
+            "Olena Hryshchenko <grischenko.olena.v@gmail.com>,\n" +
+            "Vitalii Brovko <animate28@gmail.com>,\n" +
+            "\"anna.kushnir.1008@gmail.com\" <anna.kushnir.1008@gmail.com>,\n" +
+            "Darina Shaposhnik | Guest <dshaposhnik000@gmail.com>,\n" +
+            "Dmytro Pochuiev <dmytro.pochuiev@gmail.com>,\n" +
+            "Bohdan Bezuhlyi <bezugliybohdan18012005@gmail.com>,\n" +
+            "Hreha Bohdan <grega.bogdan.v@gmail.com>,\n" +
+            "Oleg Kotenko <kotenkooleg96@gmail.com>,\n" +
+            "Maksym Trofimchuk <trofimcukmaks16@gmail.com>,\n" +
+            "Vasyl Kolesnichenko | Guest <vasilkolesnichenko1212@gmail.com>,\n" +
+            "Oleksandra Tupchiy <tupchiy.oleksandra@gmail.com>,\n" +
+            "Pavlo Dymohlo | Guest <pavelanatolevic777@gmail.com>,\n" +
+            "Petro Petrenko <petrpetrenko2023@gmail.com>,\n" +
+            "Anastasiia Voshchenko | Guest <anastasiiavoshchenko@gmail.com>,\n" +
+            "Stepan Hyryla <stepanhyryla64@gmail.com>,\n" +
+            "\"magicvova64@gmail.com\" <magicvova64@gmail.com>,\n" +
+            "Philip Kokurenchuk <filip-sumy@ukr.net>,\n" +
+            "Yuliia Suprunova | Guest <julia1suprunova@gmail.com>,\n" +
+            "Dmytro Lytvynov <ldv3007@gmail.com>,\n" +
+            "Dmytro Chornobryvets <dmitryfraire17@gmail.com>,\n" +
+            "Stanislav Zhadan <jadanstanislav05@gmail.com>,\n" +
+            "\"sashatsapko91@gmail.com\" <sashatsapko91@gmail.com>,\n" +
+            "Artem Uiechko <auechko@gmail.com>,\n" +
+            "Hennadii Perepelytsia <hennadii.perepelytsia@gmail.com>,\n" +
+            "Ostap Kutko <ostap22lviv@gmail.com>,\n" +
+            "Andrii Chyzhov <andreychizhov9@gmail.com>,\n" +
+            "Andrii Chuian <majyr295@gmail.com>,\n" +
+            "Vladyslav Sosiedskyi | Guest <vladsosedskyi@gmail.com>,\n" +
+            "Galina Tsivka <galinatsivka@gmail.com>,\n" +
+            "Yana Dumanska <dumanskayana.ua@gmail.com>,\n" +
+            "Viktor Matishak <vt.tish3@gmail.com>,\n" +
+            "Volodymyr Zhurakivskyi <zhurakv@icloud.com>,\n" +
+            "Mikhailo Bebyk <learize71@gmail.com>,\n" +
+            "\"alexyekymov@gmail.com\" <alexyekymov@gmail.com>,\n" +
+            "Anatoliy Kliushnikov <anatoliykliushnikov@gmail.com>,\n" +
+            "\"azkeep@gmail.com\" <azkeep@gmail.com>,\n" +
+            "Leonid Sazonov <leosaz06174@gmail.com>,\n" +
+            "Arsenii Lisovskyi <lisovskyiarsenii@gmail.com>,\n" +
+            "Oksana Ostrozhynska <lurayleen@gmail.com>,\n" +
+            "Anatoliy Malovaniy <malovaniyanatoliy@gmail.com>,\n" +
+            "Khrystyna Boiko <kh.boiko@gmail.com>,\n" +
+            "\"runetsdmytro2004@gmail.com\" <runetsdmytro2004@gmail.com>,\n" +
+            "Maksym Korshun <korshunmax82@gmail.com>,\n" +
+            "Vladyslav Hibskyi <varvaruk.ivan.2005@gmail.com>,\n" +
+            "\"vgibskiy@gmail.com\" <vgibskiy@gmail.com>,\n" +
+            "\"lookwithad@gmail.com\" <lookwithad@gmail.com>,\n" +
+            "\"stranger27@beroal.in.ua\" <stranger27@beroal.in.ua>,\n" +
+            "Sviatoslav Romaniv <romanivsvyatoslav2005@gmail.com>,\n" +
+            "Dmytro Fediuchok <700.dmitrij@gmail.com>,\n" +
+            "Andriy Plaksenkov <andriy.plaksenkov@gmail.com>,\n" +
+            "Yevhenii Aleshchenko <aleschenko.evgeniy97@gmail.com>,\n" +
+            "Ilona Chupa <ilonachupa@gmail.com>,\n" +
+            "BOHDANA VERESHCHAK <ellae1419@gmail.com>,\n" +
+            "Vadym Slobodianiuk <sh1gury.015@gmail.com>,\n" +
+            "Vova Diachenko <vov.dyachenko@gmail.com>,\n" +
+            "Vladyslav Soruk <sorukv@gmail.com>,\n" +
+            "\"vashchenkomichael@gmail.com\" <vashchenkomichael@gmail.com>,\n" +
+            "Olha Riabko <ruabko.olga@gmail.com>,\n" +
+            "Ivan Dackiv <getexpertonline@gmail.com>,\n" +
+            "Maksym Hrytsiuk <maxima.at.work@gmail.com>,\n" +
+            "Danylo Pechura <pechurick@gmail.com>,\n" +
+            "Nazar Kholonivets <nazarh2016v@gmail.com>,\n" +
+            "Mariana Polishchuk | Guest <tonksmaryan@gmail.com>,\n" +
+            "Alina Turchyn | Guest <turcinalina279@gmail.com>,\n" +
+            "Kateryna Malakhova <katherinemalakhova@gmail.com>,\n" +
+            "Dmytro Kuzyshyn <dimon.pumba14@gmail.com>,\n" +
+            "Igor Lamov <loginlonny@gmail.com>,\n" +
+            "\"nazariyfedorchak12214224@gmail.com\" <nazariyfedorchak12214224@gmail.com>,\n" +
+            "Vladyslav Kharkhalis <cobaka2854@gmail.com>,\n" +
+            "Maksym Martyn <maksym.martyn.shi.2024@lpnu.ua>";
+
+    public static void main(String[] args) {
+        String[] users = leaners.split(",");
+
+        System.out.println("Count :"+users.length);
+    }
+}
