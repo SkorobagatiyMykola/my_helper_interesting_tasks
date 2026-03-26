db47f46d80e15726c70061ab651687322f52157d
SkorobagatiyMykola <nick1980457@gmail.com>
2026-03-26 17:19:26 +0200

    Added patterns

---
+++ src/test/java/ua/skorobahatyi/design_patterns/behavioral_patterns/chain_of_responsibility/EmailNotifierTest.java
@@ -0,0 +1,12 @@
+package ua.skorobahatyi.design_patterns.behavioral_patterns.chain_of_responsibility;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailNotifierTest {
+
+    @Test
+    void write() {
+    }
+}
